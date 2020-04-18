b71c4dcfe4f76784ae146cc9fb9cdb18b841fca3
Danial Panah <me@danialrp.com>
2020-04-19 00:54:32 +0430

    added gateway props

--- src/Gateway/Payment.php
+++ src/Gateway/Payment.php
@@ -5,5 +5,20 @@ namespace DanialPanah\PerfectMoneyGateway\Gateway;
 
 class Payment
 {
+    /**
+     * @var string
+     */
+    private static $apiUrl = 'https://perfectmoney.com/api/step1.asp';
+
+    /**
+     * @var string
+     */
+    private static $formEncryption = 'multipart/form-data';
+
+    /**
+     * @var string
+     */
+    private $formMethod = 'POST';
+
 
 }
