957d0ee603dbd9119e3c0ce9d2053123bc640213
Danial Panah <me@danialrp.com>
2020-04-22 01:02:32 +0430

    added __set to pmGateway

--- src/PerfectMoneyGateway.php
+++ src/PerfectMoneyGateway.php
@@ -7,12 +7,47 @@ use DanialPanah\PerfectMoneyGateway\Gateway\Payment;
 class PerfectMoneyGateway
 {
     /**
-     * @param array $paymentParams
+     * @var array
+     */
+    protected $paymentDetails;
+
+
+    public function __set($name, $value)
+    {
+        $this->paymentDetails[$name] = $value;
+    }
+
+
+    /**
+     * @param null $paymentParams
      * @return Payment
      * @throws Exceptions\PerfectMoneyException
      */
-    public function initiatePaymentForm(array $paymentParams): Payment
+    public function initiatePaymentForm($paymentParams = null): Payment
     {
-        return Payment::createPaymentForm($paymentParams);
+        $paymentParams = (array)$paymentParams;
+
+        if (count($paymentParams)) {
+            $this->setPaymentDetails($paymentParams);
+        }
+
+        return $this->sendPaymentForm();
+    }
+
+    /**
+     * @param array $paymentDetails
+     */
+    private function setPaymentDetails(array $paymentDetails): void
+    {
+        $this->paymentDetails = $paymentDetails;
+    }
+
+    /**
+     * @return Payment
+     * @throws Exceptions\PerfectMoneyException
+     */
+    private function sendPaymentForm(): Payment
+    {
+        return Payment::createPaymentForm($this->paymentDetails);
     }
 }
